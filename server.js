21c6f2807c111367c7afef656c68cdc191f3c39e
Thomas Munayyer <tmunayyer@gmail.com>
2019-01-05 11:37:11 -0500

    working version

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const request = require('request');
 const app = express();
-// const cors = require("cors");
 
 app.use(express.static(__dirname));
-// app.use(cors());
 
 app.use((req, res, next) => {
   let url = req.url.split('/');
-  console.log(url);
   let path = url.slice(2).join('/');
+  console.log(path);
   let endpoint = url[1];
   if (endpoint === 'main') {
     let proxy =
       'http://ec2-3-17-128-193.us-east-2.compute.amazonaws.com/' + path;
+    console.log(proxy);
     request(proxy)
       .on('error', (err) => console.log(err))
       .pipe(res);
-    // console.log('main proxy', proxy);
   } else if (endpoint === 'details') {
     let proxy =
       'http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/' + path;
@@ -28,13 +26,14 @@ app.use((req, res, next) => {
   } else if (endpoint === 'critics') {
     let proxy =
       'http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/' + path;
-    //console.log(proxy);
     request(proxy)
       .on('error', (err) => console.log(err))
       .pipe(res);
   } else if (endpoint === 'sidebar') {
+    if (path.includes('images')) {
+      path = 'sidebar/' + path;
+    }
     let proxy = 'http://18.222.207.221:9004/' + path;
-    console.log('sidebar', proxy);
     request(proxy)
       .on('error', (err) => console.log(err))
       .pipe(res);
