bcf319966f4e1cd8963baf4b75969fcc7f1cd9fe
Thomas Munayyer <tmunayyer@gmail.com>
2019-01-05 16:19:55 -0500

    troubleshooting deployed proxy

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.use((req, res, next) => {
+  console.log(req.url);
   let url = req.url.split('/');
   let path = url.slice(2).join('/');
-  console.log(path);
   let endpoint = url[1];
   if (endpoint === 'main') {
     let proxy =
       'http://ec2-3-17-128-193.us-east-2.compute.amazonaws.com/' + path;
-    console.log(proxy);
     request(proxy)
       .on('error', (err) => console.log(err))
       .pipe(res);
@@ -25,7 +24,8 @@ app.use((req, res, next) => {
       .pipe(res);
   } else if (endpoint === 'critics') {
     let proxy =
-      'http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/' + path;
+      'http://ec2-18-218-155-141.us-east-2.compute.amazonaws.com/' + path;
+    console.log(proxy);
     request(proxy)
       .on('error', (err) => console.log(err))
       .pipe(res);
